634ea8024f375dc614330259ce6b5aa16b7a5ff2
toluwase <toluwasethomas1@gmail.com>
2021-09-23 13:48:58 +0100

    creating HTML templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,13 +1,37 @@
 package main
+
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" { http.NotFound(w, r)
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
 		return
 	}
+	// Using the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message and
+	// the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	// We then use the Execute() method on the template set to write the templa
+	// content as the response body. The last parameter to Execute() represents
+	// dynamic data that we want to pass in, which for now we'll leave as nil.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 func showSnippet(w http.ResponseWriter, r *http.Request) {
