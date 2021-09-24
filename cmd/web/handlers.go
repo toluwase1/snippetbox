9f6a1cbe89584e496a84d77604a5947ae691dd02
toluwase <toluwasethomas1@gmail.com>
2021-09-24 03:38:07 +0100

    HTML templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,16 +12,26 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+	// Initialize a slice containing the paths to the two files. Note that the
+	// home.page.tmpl file must be the *first* file in the slice.
+	files:=[]string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+	}
 	// Using the template.ParseFiles() function to read the template file into a
 	// template set. If there's an error, we log the detailed error message and
 	// the http.Error() function to send a generic 500 Internal Server Error
 	// response to the user.
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
+
+
+
+
 	// We then use the Execute() method on the template set to write the templa
 	// content as the response body. The last parameter to Execute() represents
 	// dynamic data that we want to pass in, which for now we'll leave as nil.
