63ca0912f8f0c7986c8f46814d035b4d89af89e3
toluwase <toluwasethomas1@gmail.com>
2021-09-25 06:28:48 +0100

    all styled css

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,14 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+
+	// Create a file server which serves files out of the "./ui/static" directo
+	// Note that the path given to the http.Dir function is relative to the pro
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
