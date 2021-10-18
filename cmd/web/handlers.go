842970e97589a323bc75484cd0e653094c7b5695
toluwase <toluwasethomas1@gmail.com>
2021-10-18 08:26:32 +0100

    modify handlers.go

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,61 +3,51 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
-	// Initialize a slice containing the paths to the two files. Note that the
-	// home.page.tmpl file must be the *first* file in the slice.
+
 	files := []string{
-		"./ui/html/home.page.tmpl",
+		"./ui/html/home.page.bak",
 		"./ui/html/base.layout.tmpl",
 		"./ui/html/footer.partial.tmpl",
 	}
-	// Using the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and
-	// the http.Error() function to send a generic 500 Internal Server Error
-	// response to the user.
+
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w,err)
 		return
 	}
 
-
-
-
-	// We then use the Execute() method on the template set to write the templa
-	// content as the response body. The last parameter to Execute() represents
-	// dynamic data that we want to pass in, which for now we'll leave as nil.
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-	}
-
-
-	w.Write([]byte("Hello from Snippetbox"))
-}
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.serverError(w,err)
 		return
 	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method Not Allowed", 405)
+		app.clientError(w,http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a new snippet..."))
+
 }
