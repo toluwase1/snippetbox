99e1ba8267d75dc12067595d6eb120a89a537040
toluwase <toluwasethomas1@gmail.com>
2022-04-10 08:24:23 +0100

    database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
@@ -14,12 +15,24 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL database")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	//i could have used log.Llongfile  full file path in the log output
 	//I can also force the logger to use UTC datetimes, instead of local ones, by using the log.LUTC flag
+
+	// To keep the main() function tidy I've put the code for creating a connec
+	// pool into the separate openDB() function below. We pass openDB() the DSN
+	// from the command-line flag.
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exits.
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -33,7 +46,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 
 	//err := http.ListenAndServe(*addr, mux)
 	errorLog.Fatal(err)
@@ -43,3 +56,16 @@ func main() {
 //As a rule of thumb, you should avoid using the Panic() and Fatal()
 //variations outside of your main() function — it’s good practice to
 //return errors instead, and only panic or exit directly from main().
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
